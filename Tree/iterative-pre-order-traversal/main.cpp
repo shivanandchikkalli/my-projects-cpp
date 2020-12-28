a735a34ac787f4dc0f8edb774987409b6914e962
Shivanand <shivanan@deloitte.com>
2020-12-28 22:16:56 +0530

    iterative traversal of tree

---
+++ Tree/iterative-pre-order-traversal/main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<queue>
+
+#include "tree.h"
+
+int main()
+{
+    std::queue<Node*> queue;
+
+    Tree tree;
+
+    bool exitTreeCreation = false;
+
+    int userInput = 0;
+
+    std::cout << "Enter root element" << std::endl;
+    std::cin >> userInput;
+
+    queue.push(tree.addRoot(userInput));
+
+    Node* temp = NULL;
+
+
+    while(!exitTreeCreation)
+    {
+        std::cout << "press -1 to stop adding tree elements..." << std::endl;
+        std::cin >> userInput;
+        if(userInput == -1)
+            break;
+        temp = queue.front();
+        queue.pop();
+
+        std::cout << "Enter left element of " << temp->element << std::endl;
+        std::cin >> userInput;
+
+        queue.push(tree.addLeft(temp, userInput));
+
+        std::cout << "Enter right element of " << temp->element << std::endl;
+        std::cin >> userInput;
+
+        queue.push(tree.addRight(temp, userInput));
+
+    }
+
+    tree.printTree(tree.getRoot());
+    
+    return 0;
+}
