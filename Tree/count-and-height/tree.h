35ada7b9e1d9fd44368a1b3904e5067b9ec0d981
Shivanand <shivanan@deloitte.com>
2020-12-29 12:01:45 +0530

    count of tree - recursive method

---
+++ Tree/count-and-height/tree.h
@@ -0,0 +1,33 @@
+#ifndef _H_TREE_
+#define _H_TREE_
+
+// This is just a simple tree creation code and display the created tree using inorder traversal method
+// Contains only add methods (left and right)
+
+typedef struct Node
+{
+    int element;
+    Node* parent;
+    Node* left;
+    Node* right;
+}Node;
+
+class Tree
+{
+    private:
+        Node* root;
+        int size;
+    public:
+        Tree();
+        ~Tree();
+        Node* getRoot() const;
+        int size_of_tree() const;
+        Node* addRoot(int value);
+        Node* addLeft(Node* node, int value);
+        Node* addRight(Node* node, int value);
+        void printTree(Node* node) const;
+        int count(Node* node) const;
+};
+
+
+#endif
