85a7315c5c12277ccf5051dd392e373166d1dc6b
Shivanand <shivanan@deloitte.com>
2021-01-02 01:03:30 +0530

    Update main.cpp
    other function calls uncommented.

--- Tree/avl-tree/main.cpp
+++ Tree/avl-tree/main.cpp
@@ -38,16 +38,16 @@ int main()
 
     std::cout << std::endl << "Height() => " << tree.height(tree.getRoot()) << std::endl;
 
-    // std::cout << "Enter element to search" << std::endl;
-    // std::cin >> userInput;
+    std::cout << "Enter element to search" << std::endl;
+    std::cin >> userInput;
 
-    // std::cout << "Iterative Search Result : " << tree.Search(userInput) << std::endl;
-    // std::cout << "Recursive Search Result : " << tree.RSearch(tree.getRoot(), userInput) << std::endl;
+    std::cout << "Iterative Search Result : " << tree.Search(userInput) << std::endl;
+    std::cout << "Recursive Search Result : " << tree.RSearch(tree.getRoot(), userInput) << std::endl;
 
-    // std::cout << "In-Order Predecessor : " << tree.Get_inorder_predecessor(tree.getRoot()) << std::endl;
-    // std::cout << "In-Order successor : " << tree.Get_inorder_successor(tree.getRoot()) << std::endl;
+    std::cout << "In-Order Predecessor : " << tree.Get_inorder_predecessor(tree.getRoot()) << std::endl;
+    std::cout << "In-Order successor : " << tree.Get_inorder_successor(tree.getRoot()) << std::endl;
 
-    // tree.printTree(tree.getRoot());
+    tree.printTree(tree.getRoot());
 
     std::cout << "Enter element to Delete" << std::endl;
     std::cin >> userInput;
