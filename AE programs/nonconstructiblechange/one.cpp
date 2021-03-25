1ebd3d66f4844ae77137d4ebafdc64c2d730feed
Shivanand Chikkalli <sychikkalli@gmail.com>
2021-03-25 23:26:12 +0530

    fifth problem

---
+++ AE programs/nonconstructiblechange/one.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/* Given an array of positive integers representing the values of coins in your
+  possession, write a function that returns the minimum amount of change (the
+  minimum sum of money) that you cannot create. The given coins can have
+  any positive integer value and aren't necessarily unique (i.e., you can have
+  multiple coins of the same value). */
+// This solution takes O(nlogn) time and O(1) space
+int nonConstructibleChange(vector<int> coins) {
+  
+	std::sort(coins.begin(), coins.end());
+	
+	int smallestSum = 1;
+	
+	for(int val : coins)
+	{
+		if(val > smallestSum)
+			return smallestSum;
+		else if(val <= smallestSum)
+			smallestSum += val;
+	}
+	
+  return smallestSum;
+}
